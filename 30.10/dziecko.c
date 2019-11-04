d71881e565d9a643f81db7046aae687f34a979ac
Kinga <containedx@niu.ula.a@gmail.com>
2019-11-04 23:19:50 +0100

    it starts make sense

--- 30.10/dziecko.c
+++ 30.10/dziecko.c
@@ -6,13 +6,12 @@
 int main( int argc, char** argv )
 {
 	//int time = strtol(argv[1], 0, 10); 
-	//printf("%d <- tyle nanosekund czekam ", time );
+	//printf("%d <- tyle nanosekund powinnam czekac\n ", time );
+	
 	struct timespec ts = { 1, 0 }; 
 	nanosleep( &ts, NULL); 
 	printf("*** hej, jestem dziecko *** \n");
-	nanosleep( &ts, NULL);
-	printf("bye\n"); 
-        
+	      
 
    return 0;  
 }
--- 30.10/rodzic.c
+++ 30.10/rodzic.c
@@ -2,39 +2,80 @@
 #include <stdlib.h> 
 #include <unistd.h>
 #include <sys/types.h> 
+#include <time.h>
+
+// to jest rodzic. Tworzy on P0, L1 i L2. 
+// P0 generuje P1 i P2
 
 int main(int argc, char** argv)
 {
 	int opt;
-	float ltime, ptime, rtime; 
+	char *ltime, *ptime, *rtime;
 
-	while( (opt = getopt( argc, argv, "l:p:r:" ) != -1 ))
+	while( (opt = getopt( argc, argv, "l:p:r:" ) != -1 )) 
+		// to ma pobierac jako argumenty czas, do dopracowania
 		{
 		switch(opt) 
 		{
 			case 'l':
-				ltime = strtof( optarg, 0 );
+				ltime = optarg; // = strtof( optarg, 0 );
 				break;
 			case 'p':
-				ptime = strtof( optarg, 0 ); 
-				break; 
+				ptime = optarg; //= strtof( optarg, 0 ); 
+		;		break; 
 			case 'r':
-				rtime = strtof( optarg, 0 ); 
+				rtime = optarg;  //= strtof( optarg, 0 ); 
 				break; 
 		}
 
 		}
 
-	fork();	
-	if( fork() == 0 )
+	pid_t p0, l1, l2; 
+	
+	//fork(); 
+
+	switch( l1 = fork())
 	{
-		fork(); 
-		execl( "kid", NULL); //jesli dziecko to exec
+		case -1:
+			return -1;
+		case 0:
+			printf("egzekuje dziecko l1\n");
+			execl("kid", ltime);
+		default: 
+			sleep(1);
+			printf("spie, elo ~ l1\n");
+			break;
 	}
-	else 
-		printf("\nxxx hello iam parent xxx\n"); 
 
+	switch( l2 = fork() )
+	{
+		case -1:
+			return -1;
+		case 0: 
+			printf("egzekuje dziecko l2\n");
+			execl("kid", ltime); 
+		default: 
+			sleep(1);
+			printf("spie, elo ~l2\n");
+			break;;;
+	}
 
+	switch( p0 = fork()) 
+	{
+		case -1:
+			return -1; 
+		case 0:
+			fork();
+			printf("jestem p0 i tworze teraz potomka p1 lub p2\n");
+			execl("kid", ptime );
+			break;
+		default: 
+			sleep( 1 );
+		        printf("spie, elo, tu p0\n"); 	
+			break;
+	}
+
+	
 
 	return 0; 
 }
